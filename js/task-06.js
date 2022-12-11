0414cac97b3f155b1170dbfd06d97a38dc76ceeb
Oleksandr Shmatko <blacknorfolk@gmail.com>
2022-12-11 23:33:06 +0200

    fix 10 task

--- js/task-06.js
+++ js/task-06.js
@@ -4,10 +4,11 @@ inputEl.addEventListener('blur', onInputBlur);
 
 function onInputBlur(event) {
   const { value, dataset } = event.currentTarget;
-  if (value.length >= dataset.length) {
+  if (value.length === dataset.length) {
     inputEl.classList.remove('invalid');
     inputEl.classList.add('valid');
   } else {
+    inputEl.classList.remove('valid');
     inputEl.classList.add('invalid');
   }
 }
--- js/task-10.js
+++ js/task-10.js
@@ -2,23 +2,24 @@ const inputEl = document.querySelector('input');
 const createBtn = document.querySelector('button[data-create]');
 const destroyBtn = document.querySelector('button[data-destroy]');
 const boxesEl = document.querySelector('#boxes');
+let dimentions = 30;
 
 createBtn.addEventListener('click', onCreateBtnClick);
 destroyBtn.addEventListener('click', onDestroyBtnClick);
 
 function onCreateBtnClick() {
-  let dimentions = 30;
   let markup = '';
   const elements = inputEl.value;
   for (let i = elements; i > 0; i -= 1) {
     markup += `<div style="width: ${dimentions}px; height: ${dimentions}px; background-color: ${getRandomHexColor()};"></div>`;
     dimentions += 10;
   }
-  boxesEl.insertAdjacentHTML('afterbegin', markup);
+  boxesEl.insertAdjacentHTML('beforeend', markup);
 }
 
 function onDestroyBtnClick() {
   boxesEl.innerHTML = '';
+  dimentions = 30;
 }
 
 function getRandomHexColor() {
