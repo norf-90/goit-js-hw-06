f77f6f513013a8bbe5152c9fabe0d349005ae6ae
Oleksandr Shmatko <blacknorfolk@gmail.com>
2022-12-12 11:40:12 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -4,8 +4,7 @@ inputEl.addEventListener('blur', onInputBlur);
 
 function onInputBlur(event) {
   const { value, dataset } = event.currentTarget;
-  console.log(typeof value.length);
-  console.log(typeof dataset.length);
+
   if (value.length == dataset.length) {
     inputEl.classList.remove('invalid');
     inputEl.classList.add('valid');
